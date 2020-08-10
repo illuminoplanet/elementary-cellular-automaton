46d57575f1e7338bccea4506b48e3ba9a7cb4139
JooyoungKim <illuminoplanet@gmail.com>
2020-08-10 23:55:21 +0900

    Organized folder

---
+++ UI/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+import ElementaryCellularAutomaton from './ElementaryCellularAutomaton/ElementaryCellularAutomaton.jsx'
+
+
+class App extends Component {
+  render() { 
+    return (
+      <ElementaryCellularAutomaton></ElementaryCellularAutomaton>
+    );
+  }
+}
+
+export default App;
