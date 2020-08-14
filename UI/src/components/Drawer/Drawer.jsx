f09bd42135beab2a0fa235d7d64a1cc82fe03ab9
JooyoungKim <illuminoplanet@gmail.com>
2020-08-14 21:24:47 +0900

    Building rule switch

--- UI/src/components/Drawer/Drawer.jsx
+++ UI/src/components/Drawer/Drawer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Backdrop from "./Backdrop";
+import RuleSwitch from "./RuleSwitch";
 import "./Drawer.css";
 
 export default class Drawer extends Component {
@@ -12,7 +13,17 @@ export default class Drawer extends Component {
             : "drawer-window";
         return (
             <div className="drawer">
-                <div className={className}></div>
+                <div className={className}>
+                    <RuleSwitch pattern={"111"}></RuleSwitch>
+                    <RuleSwitch pattern={"110"}></RuleSwitch>
+                    <RuleSwitch pattern={"101"}></RuleSwitch>
+                    <RuleSwitch pattern={"100"}></RuleSwitch>
+                    <RuleSwitch pattern={"011"}></RuleSwitch>
+                    <RuleSwitch pattern={"010"}></RuleSwitch>
+                    <RuleSwitch pattern={"001"}></RuleSwitch>
+                    <RuleSwitch pattern={"000"}></RuleSwitch>
+                </div>
+
                 {backdrop}
             </div>
         );
---
+++ UI/src/components/Drawer/RuleSwitch.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./RuleSwitch.css";
+
+export default class RuleSwitch extends Component {
+    render() {
+        return <button className="rule-switch"></button>;
+    }
+}
