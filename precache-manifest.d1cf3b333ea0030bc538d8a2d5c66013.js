ac3ca83a6eb99811fd37d3aa9b6ca58c0d3055e5
JooyoungKim <illuminoplanet@gmail.com>
2020-09-29 15:49:29 +0900

    Updates

---
+++ precache-manifest.d1cf3b333ea0030bc538d8a2d5c66013.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e1bd43b0a4f61b211d29903ca4c267",
+    "url": "/Elementary-Cellular-Automaton/index.html"
+  },
+  {
+    "revision": "fa7341cb4121f05617e8",
+    "url": "/Elementary-Cellular-Automaton/static/css/main.ebfe7eef.chunk.css"
+  },
+  {
+    "revision": "6246acdb778b827a9bc8",
+    "url": "/Elementary-Cellular-Automaton/static/js/2.2a7ecebb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Elementary-Cellular-Automaton/static/js/2.2a7ecebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa7341cb4121f05617e8",
+    "url": "/Elementary-Cellular-Automaton/static/js/main.81fe083e.chunk.js"
+  },
+  {
+    "revision": "b64a6568b9b4d6da7246",
+    "url": "/Elementary-Cellular-Automaton/static/js/runtime-main.331b7697.js"
+  },
+  {
+    "revision": "0769ca7ffcc19d2d2183228953d08ec6",
+    "url": "/Elementary-Cellular-Automaton/static/media/button_reset.0769ca7f.svg"
+  },
+  {
+    "revision": "bcd64ce671b0abae40c7eef2548e576a",
+    "url": "/Elementary-Cellular-Automaton/static/media/button_start.bcd64ce6.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Elementary-Cellular-Automaton/precache-manifest.d1cf3b333ea0030bc538d8a2d5c66013.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Elementary-Cellular-Automaton/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
