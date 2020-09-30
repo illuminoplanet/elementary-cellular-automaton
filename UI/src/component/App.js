21e935d455177b604421ceee85699b359b065c10
JooyoungKim <illuminoplanet@gmail.com>
2020-09-30 23:22:48 +0900

    Adjusted server request

--- UI/src/component/App.js
+++ UI/src/component/App.js
@@ -33,8 +33,6 @@ export default class App extends Component {
         fetch(
             "https://5cof2ds052.execute-api.us-east-1.amazonaws.com/elementary-cellular-automaton/get-grid",
             {
-                headers:
-                    "Origin: https://illuminoplanet.github.io/elementary-cellular-automaton/",
                 method: "POST",
                 mode: "cors",
                 body: JSON.stringify({
