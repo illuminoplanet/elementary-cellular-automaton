31895a12db8e51e2c0f7d85107fbdf80650b58e0
JooyoungKim <illuminoplanet@gmail.com>
2020-09-30 23:29:08 +0900

    Updates

--- precache-manifest.fb7c15ed112dbfde0bd6beebde5ba978.js
+++ precache-manifest.fb7c15ed112dbfde0bd6beebde5ba978.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "76fe4ba7734cb8410810922b8ff50c82",
+    "revision": "28e02f6b28e82d44a7bd98b4feee6f22",
     "url": "/elementary-cellular-automaton/index.html"
   },
   {
-    "revision": "631d10c08da061f4aa31",
+    "revision": "ca7a6ef83dca6785b1e6",
     "url": "/elementary-cellular-automaton/static/css/main.07d0ff88.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/elementary-cellular-automaton/static/js/2.2a7ecebb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "631d10c08da061f4aa31",
-    "url": "/elementary-cellular-automaton/static/js/main.e4428c54.chunk.js"
+    "revision": "ca7a6ef83dca6785b1e6",
+    "url": "/elementary-cellular-automaton/static/js/main.4682b1d2.chunk.js"
   },
   {
     "revision": "c1e75352e6b470a0e4e4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/elementary-cellular-automaton/precache-manifest.184890650d5d627b6ba33f9fbfb404d1.js"
+  "/elementary-cellular-automaton/precache-manifest.fb7c15ed112dbfde0bd6beebde5ba978.js"
 );
 
 self.addEventListener('message', (event) => {
